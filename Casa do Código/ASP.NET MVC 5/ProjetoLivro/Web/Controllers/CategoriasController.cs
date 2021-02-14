52d27b4d5e4a1f3ffe1e235b16db637967e168b1
Danilo Silva <danilo.silva@msn.com>
2021-02-14 17:41:24 -0300

    Criação de uma lista para simular dados do BD

--- Casa do Código/ASP.NET MVC 5/ProjetoLivro/Web/Controllers/CategoriasController.cs
+++ Casa do Código/ASP.NET MVC 5/ProjetoLivro/Web/Controllers/CategoriasController.cs
@@ -1,9 +1,20 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Web.Models;
 
 namespace Web.Controllers
 {
     public class CategoriasController : Controller
     {
+        private static readonly IList<Categoria> categorias = new List<Categoria>()
+            {
+                new Categoria() { Id = 1, Nome = "Notebooks" },
+                new Categoria() { Id = 2, Nome = "Monitores" },
+                new Categoria() { Id = 3, Nome = "Impressoras" },
+                new Categoria() { Id = 4, Nome = "Mouses" },
+                new Categoria() { Id = 5, Nome = "Desktops" }
+            };
+
         public ActionResult Index()
         {
             return View();

daa170adf20f418f1ba993487f6df8ecf0afa323
Danilo Silva <danilo.silva@msn.com>
2021-02-14 20:16:20 -0300

    Configuração do Contexto e Criação da Controller Fabricantes

---
+++ Casa do Código/ASP.NET MVC 5/ProjetoLivro/Web/Contexts/EFContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using Web.Models;
+
+namespace Web.Contexts
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("Asp_Net_MVC_CS") { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+    }
+}
---
+++ Casa do Código/ASP.NET MVC 5/ProjetoLivro/Web/Controllers/FabricantesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Web.Contexts;
+
+namespace Web.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private EFContext context = new EFContext();
+
+        public ActionResult Index()
+        {
+            return View(context.Fabricantes.OrderBy(c => c.Nome));
+        }
+    }
+}

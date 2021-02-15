c9a7e700da586c56f2234901d4d6f9073b086111
Danilo Silva <danilo.silva@msn.com>
2021-02-14 23:20:36 -0300

    Adaptação às associações com Produtos x Categorias x Fabricantes

--- Casa do Código/ASP.NET MVC 5/ProjetoLivro/Web/Contexts/EFContext.cs
+++ Casa do Código/ASP.NET MVC 5/ProjetoLivro/Web/Contexts/EFContext.cs
@@ -5,9 +5,12 @@ namespace Web.Contexts
 {
     public class EFContext : DbContext
     {
-        public EFContext() : base("Projeto_Livro_AspNet") { }
+        public EFContext() : base("Projeto_Livro_AspNet") {
+            Database.SetInitializer<EFContext>(new DropCreateDatabaseIfModelChanges<EFContext>());
+        }
 
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Fabricante> Fabricantes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
     }
 }
--- Casa do Código/ASP.NET MVC 5/ProjetoLivro/Web/Models/Categoria.cs
+++ Casa do Código/ASP.NET MVC 5/ProjetoLivro/Web/Models/Categoria.cs
@@ -1,8 +1,12 @@
-﻿namespace Web.Models
+﻿using System.Collections.Generic;
+
+namespace Web.Models
 {
     public class Categoria
     {
         public long Id { get; set; }
         public string Nome { get; set; }
+
+        public virtual ICollection<Produto> Produtos { get; set; }
     }
 }
